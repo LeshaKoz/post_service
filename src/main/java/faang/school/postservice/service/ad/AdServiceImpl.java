9ec7e74283882333f732c21f60f6614edb39133b
Maria Bortsova <bortsovamariia@gmail.com>
2025-02-18 20:46:05 +0300

    BJS2-53120: fix

--- src/main/java/faang/school/postservice/service/ad/AdServiceImpl.java
+++ src/main/java/faang/school/postservice/service/ad/AdServiceImpl.java
@@ -2,7 +2,9 @@ package faang.school.postservice.service.ad;
 
 import faang.school.postservice.model.ad.Ad;
 import faang.school.postservice.repository.ad.AdRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -10,12 +12,14 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AdServiceImpl implements AdService {
 
     private final AdRepository adRepository;
 
+    @Transactional
     @Override
     public void deleteExpiredAds() {
         Pageable limit = PageRequest.of(0, 10);
