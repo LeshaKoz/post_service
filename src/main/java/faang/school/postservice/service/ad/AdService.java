fa2677084337d313ec820720c4152680a1724213
Dias Kamza <kamzadias31@gmail.com>
2025-02-21 10:58:12 +0500

    BJS2-53851 fixed by comments

--- src/main/java/faang/school/postservice/service/ad/AdService.java
+++ src/main/java/faang/school/postservice/service/ad/AdService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
