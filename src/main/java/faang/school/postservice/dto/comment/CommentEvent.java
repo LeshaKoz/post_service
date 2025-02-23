55011a857dfe70b36c200e2114bc6edf0e3c0554
Lesha Kozyrev <alekseykoz@gmail.com>
2025-02-23 14:03:32 +0300

    CommentEventPublisher was added to CommentServiceImpl.

--- src/main/java/faang/school/postservice/dto/comment/CommentEvent.java
+++ src/main/java/faang/school/postservice/dto/comment/CommentEvent.java
@@ -1,14 +1,17 @@
 package faang.school.postservice.dto.comment;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 public record CommentEvent(
+        Long postAuthorId,
+        Long commentAuthorId,
         Long postId,
-        Long authorId,
         Long commentId,
-        LocalDateTime timestamp
+        LocalDateTime commentedAt
 ) {
 }
