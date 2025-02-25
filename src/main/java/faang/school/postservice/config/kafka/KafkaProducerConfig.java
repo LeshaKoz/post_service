7848c6494a50a26d5ca768fbb7795ae329e35385
Ivan Baev <ibai86@yandex.ru>
2025-02-25 16:52:45 +0300

    producer ready

--- src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
+++ src/main/java/faang/school/postservice/config/kafka/KafkaProducerConfig.java
@@ -1,6 +1,7 @@
 package faang.school.postservice.config.kafka;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,10 +18,13 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
--- src/main/java/faang/school/postservice/event/PostViewEvent.java
+++ src/main/java/faang/school/postservice/event/PostViewEvent.java
@@ -1,10 +1,9 @@
 package faang.school.postservice.event;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Getter
@@ -12,16 +11,8 @@ import java.time.LocalDateTime;
 @Builder
 public class PostViewEvent {
 
-    @NotNull
-    @Positive
     private final Long postId;
-
-    @NotNull
-    @Positive
     private final Long authorId;
-
-    @NotNull
-    @Positive
     private final Long userId;
 
     @Builder.Default
