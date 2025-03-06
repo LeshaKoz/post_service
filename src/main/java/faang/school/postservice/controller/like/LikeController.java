becd2a0ffda07e102d85a94796b41c27d8caec75
Roman Shevchuk <reacount1393@gmail.com>
2025-03-07 01:14:00 +0300

    Ребейз сделан, тесты работают

--- src/main/java/faang/school/postservice/controller/like/LikeController.java
+++ src/main/java/faang/school/postservice/controller/like/LikeController.java
@@ -4,16 +4,23 @@ import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.service.like.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.Positive;
 
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/likes")
 public class LikeController {
 
@@ -33,5 +40,27 @@ public class LikeController {
         List<UserDto> users = likeService.getUsersWhoLikedComment(commentId);
         log.info("Returned {} users who liked comment with id: {}", users.size(), commentId);
         return users;
+    }
+
+    @PostMapping("/post/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void likeForPost(@PathVariable("id") Long postId) {
+        likeService.createLikeForPost(postId);
+    }
+
+    @PostMapping("/comment/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void likeForComment(@PathVariable("id") Long commentId) {
+        likeService.createLikeForComment(commentId);
+    }
+
+    @DeleteMapping("/post/{postId}")
+    public void deleteLikeFromPost(@PathVariable @Positive Long postId) {
+        likeService.deleteLikeFromPost(postId);
+    }
+
+    @DeleteMapping("/comment/{commentId}")
+    public void deleteLikeFromComment(@PathVariable @Positive Long commentId) {
+        likeService.deleteLikeFromComment(commentId);
     }
 }
