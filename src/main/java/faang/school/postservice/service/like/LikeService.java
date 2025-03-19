abc034597d412e8ecb4162d81be16a526806a472
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-19 15:39:54 +0500

    https://faang-school.atlassian.net/browse/BJS2-65240 LikeService + add 2 method from LikeRepository

--- src/main/java/faang/school/postservice/service/like/LikeService.java
+++ src/main/java/faang/school/postservice/service/like/LikeService.java
@@ -3,22 +3,17 @@ package faang.school.postservice.service.like;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserDto;
 import faang.school.postservice.model.Like;
-import faang.school.postservice.repository.CommentRepository;
 import faang.school.postservice.repository.LikeRepository;
-import faang.school.postservice.repository.PostRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
     private final LikeRepository likeRepository;
-    private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
     private final UserServiceClient userServiceClient;
 
     @Transactional
