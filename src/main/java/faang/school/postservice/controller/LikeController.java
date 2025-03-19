a8f30e551deb73c97a541f18715140a9557d7457
Dmitry Rogulkin <rogulkind@yandex.ru>
2025-03-19 12:51:47 +0500

    https://faang-school.atlassian.net/browse/BJS2-65240 LikeService

--- src/main/java/faang/school/postservice/controller/LikeController.java
+++ src/main/java/faang/school/postservice/controller/LikeController.java
@@ -1,4 +1,29 @@
 package faang.school.postservice.controller;
 
+/*
+создать два метода:
+один для получения всех лайкнувших пользователей по id поста,
+а второй — для получения всех лайкнувших пользователей по id коммента.
+ */
+
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.model.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/getlike")
+@RequiredArgsConstructor
 public class LikeController {
+    private final LikeService likeService;
+
+    @GetMapping("/{authorId}")
+    public List<UserDto> getLikesByPostId(@PathVariable Long postId) {
+        return likeService.getLikesByPostId(postId);
+    }
 }
---
+++ src/main/java/faang/school/postservice/service/like/LikeService.java
@@ -0,0 +1,24 @@
+package faang.school.postservice.service.like;
+
+import faang.school.postservice.dto.user.UserDto;
+import faang.school.postservice.model.Post;
+import faang.school.postservice.repository.CommentRepository;
+import faang.school.postservice.repository.LikeRepository;
+import faang.school.postservice.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+    private final LikeRepository likeRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    public List<UserDto> getUserLikedPost(long postId) {
+
+        return null;
+    }
+}
