673e0825ae99a246cfe8fadf1b91c25139541d68
bambum <gwalka885@gmai.com>
2025-03-20 16:07:00 +0300

    внесене исправлений ServiceTest++

---
+++ src/main/java/faang/school/postservice/controller/CommentController.java
@@ -0,0 +1,44 @@
+package faang.school.postservice.controller;
+
+import faang.school.postservice.dto.comment.CommentDto;
+import faang.school.postservice.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService service;
+
+    @PostMapping("/create")
+    public CommentDto createComment(
+            @PathVariable long userId, @PathVariable long postId, @RequestBody CommentDto commentDto) {
+        return service.createComment(userId, postId, commentDto);
+    }
+
+    @PutMapping("/edit/{commentId}")
+    public CommentDto editComment(
+            @RequestBody CommentDto commentDto, @PathVariable long commentId, @PathVariable String content) {
+        return service.editComment(commentDto, commentId, content);
+    }
+
+    @GetMapping("/post/{postId}")
+    public List<CommentDto> getAllComments(@PathVariable long postId) {
+        return service.getAllComments(postId);
+    }
+
+    @DeleteMapping("/delete/{commentId}")
+    public void deleteComment(@PathVariable long commentId) {
+        service.deleteComment(commentId);
+    }
+}
