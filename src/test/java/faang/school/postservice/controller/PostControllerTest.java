ea1891999db0262505604d7d84d44971392f813e
Mipusatina <mipspam@mail.ru>
2025-03-23 19:55:39 +0100

    PostControllerTest

--- src/test/java/faang/school/postservice/controller/PostControllerTest.java
+++ src/test/java/faang/school/postservice/controller/PostControllerTest.java
@@ -1,17 +1,40 @@
 package faang.school.postservice.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import faang.school.postservice.dto.post.PostDto;
+import faang.school.postservice.mapper.PostMapper;
+import faang.school.postservice.model.Post;
+import faang.school.postservice.service.PostService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PostControllerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Mock
+    private PostService postService;
+    @Mock
+    private PostMapper postMapper;
+
+    @InjectMocks
+    private PostController postController;
 
     @Test
-    void getComment() {
+    void getPost() {
+        Long postId = 123765L;
+        Post post = new Post();
+        PostDto dto = new PostDto();
+
+        Mockito.when(postService.getPost(postId)).thenReturn(post);
+        Mockito.when(postMapper.toDto(Mockito.any(Post.class))).thenReturn(dto);
+
+        assertEquals(dto, postController.getPost(postId));
+        Mockito.verify(postService, Mockito.times(1)).getPost(Mockito.any());
+        Mockito.verify(postMapper, Mockito.times(1)).toDto(Mockito.any());
     }
 }
