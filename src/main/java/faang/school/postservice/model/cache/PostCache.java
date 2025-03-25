1caa4995a77e8fbf85090dfdd23ff76d7c894a82
rostex <rostex2@yandex.ru>
2025-03-25 21:35:34 +0300

    Реализовано сохранение в кэш постов после публикации

---
+++ src/main/java/faang/school/postservice/model/cache/PostCache.java
@@ -0,0 +1,24 @@
+package faang.school.postservice.model.cache;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("post")
+public class PostCache {
+    @Id
+    private long id;
+    private long authorId;
+    private String content;
+    private int likesAmount;
+    private LocalDateTime createdAt;
+}
