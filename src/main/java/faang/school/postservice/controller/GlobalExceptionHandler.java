c860a82340a749a503ddea1e7a96bf40d6757f98
Ilhan Mustafa <ilhnmst@gmail.com>
2025-03-25 16:03:00 +0300

    Add logging

--- src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package faang.school.postservice.controller;
 import faang.school.postservice.exception.DataValidationException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,21 +14,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataValidationException.class)
     public ResponseEntity<String> handleDataValidationException(DataValidationException ex) {
+        log.error("Обработано исключение валидации: " + ex.getMessage(), ex);
         return BadRequest(ex);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        log.error("Обработано исключение отсутствия сущности: " + ex.getMessage(), ex);
         return NotFound(ex);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
+        log.error("Обработано исключение нарушения ограничений: " + ex.getMessage(), ex);
         return BadRequest(ex);
     }
 
@@ -39,11 +44,13 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        log.error("Обработано исключение валидации аргументов метода: " + ex.getMessage(), ex);
         return ResponseEntity.badRequest().body(errors);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        log.error("Обработано исключение в ходе работы программы: " + ex.getMessage(), ex);
         return InternalServerError(ex);
     }
 
