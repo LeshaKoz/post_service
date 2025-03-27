a836ffacd40e7ce8e19c929265d0bc87a40dc9d4
AesaQ <ilyaovchinnikov2095@gmail.com>
2025-03-27 12:15:49 +0500

    added thread pool for cachePost

--- src/main/java/faang/school/postservice/config/AsyncConfig.java
+++ src/main/java/faang/school/postservice/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -9,12 +10,21 @@ import java.util.concurrent.Executor;
 @Configuration
 public class AsyncConfig {
 
+    @Value("${spring.data.redis.cache.post.core-pool-size}")
+    private int cachePostCorePoolSize;
+
+    @Value("${spring.data.redis.cache.post.max-pool-size}")
+    private int cachePostMaxPoolSize;
+
+    @Value("${spring.data.redis.cache.post.queue-capacity}")
+    private int cachePostQueueCapacity;
+
     @Bean(name = "cachePostExecutor")
     public Executor cachePostExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(500);
+        executor.setCorePoolSize(cachePostCorePoolSize);
+        executor.setMaxPoolSize(cachePostMaxPoolSize);
+        executor.setQueueCapacity(cachePostQueueCapacity);
         executor.setThreadNamePrefix("cache-post-");
         executor.initialize();
         return executor;
