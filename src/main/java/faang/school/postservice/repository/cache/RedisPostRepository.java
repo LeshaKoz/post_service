b2f0edca31dbb18ba33ecb7ac02bc5c23559b33c
Aleksandr Kozlov <vismut-888@yandex.ru>
2025-03-29 14:25:16 +0300

    Изменил repository

--- src/main/java/faang/school/postservice/repository/cache/RedisPostRepository.java
+++ src/main/java/faang/school/postservice/repository/cache/RedisPostRepository.java
@@ -1,9 +1,10 @@
 package faang.school.postservice.repository.cache;
 
 import faang.school.postservice.model.cache.PostCache;
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RedisPostRepository extends CrudRepository<PostCache, Long> {
+public interface RedisPostRepository extends KeyValueRepository<PostCache, Long> {
 }
--- src/main/java/faang/school/postservice/repository/cache/RedisUserRepository.java
+++ src/main/java/faang/school/postservice/repository/cache/RedisUserRepository.java
@@ -1,9 +1,10 @@
 package faang.school.postservice.repository.cache;
 
 import faang.school.postservice.model.cache.UserCache;
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RedisUserRepository extends CrudRepository<UserCache, Long> {
+public interface RedisUserRepository extends KeyValueRepository<UserCache, Long> {
 }
