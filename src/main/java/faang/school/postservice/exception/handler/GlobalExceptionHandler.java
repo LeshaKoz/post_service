c2171d4b165a68dda30d6cd9044fa196e1d6a4c2
SergeyErmoshenko <ermoshenko.work@gmail.com>
2025-03-30 18:21:23 +0300

    добавил исключения в handler

--- src/main/java/faang/school/postservice/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/faang/school/postservice/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package faang.school.postservice.exception.handler;
 
+import faang.school.postservice.exception.InvalidPostAuthorsException;
 import faang.school.postservice.exception.LanguageToolException;
+import faang.school.postservice.exception.PostNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,5 +20,35 @@ public class GlobalExceptionHandler {
                 .property("service", "LanguageTool")
                 .build();
         return new ResponseEntity<>(errorResponse, exception.getStatusCode());
+    }
+
+    @ExceptionHandler(InvalidPostAuthorsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidPostAuthorsException(InvalidPostAuthorsException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST,
+                        exception.getMessage())
+                .title("Invalid post authors error")
+                .property("service", "PostService")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePostNotFoundException(PostNotFoundException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST,
+                        exception.getMessage())
+                .title("Post not found")
+                .property("service", "PostService")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
+        ErrorResponse errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST,
+                        exception.getMessage())
+                .title("Invalid request data")
+                .property("service", "PostService")
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
