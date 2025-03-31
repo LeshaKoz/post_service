11018a288f92acbbde69f48b12995ea900c30f75
Lesha Kozyrev <alekseykoz@gmail.com>
2025-03-31 21:04:57 +0300

    Files were updated.

--- src/main/java/faang/school/postservice/client/UserServiceClient.java
+++ src/main/java/faang/school/postservice/client/UserServiceClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}/${user-service.version}")
+@FeignClient(name = "user-service", url = "${user-service.host}:${user-service.port}${user-service.version}")
 public interface UserServiceClient {
 
     @GetMapping("/users/{userId}")
