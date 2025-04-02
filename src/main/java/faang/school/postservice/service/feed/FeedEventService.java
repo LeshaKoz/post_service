9cbb82af4a3d07f82ddee31aefe2306dde59b42b
Lesha Kozyrev <alekseykoz@gmail.com>
2025-04-02 23:55:43 +0300

    Corrected by comments.

--- src/main/java/faang/school/postservice/service/feed/FeedEventService.java
+++ src/main/java/faang/school/postservice/service/feed/FeedEventService.java
@@ -40,20 +40,17 @@ public class FeedEventService {
         log.info("subscribersIds {} ", subscribersIds);
         if (subscribersIds.isEmpty()) {
             log.info("Author {} has no subscribers or failed to retrieve subscribers. No events will be sent.", authorId);
-        } else if (subscribersIds.size() <= properties.getSubscribersBatchSize()) {
-            kafkaPostProducer.sendEventToTopic(new FeedPostEvent(postId, authorId, publishedAt, subscribersIds), topicName);
-            log.info("Sent FeedPostEvent for postId {} with subscribers {}", postId, subscribersIds);
         } else {
             List<List<Long>> batches = ListUtils.partition(subscribersIds, properties.getSubscribersBatchSize());
-
-            AtomicInteger batchNumber = new AtomicInteger();
-            batches.forEach(batch -> {
+            int batchNumber = 0;
+            for (List<Long> batch : batches) {
                 FeedPostEvent event = new FeedPostEvent(postId, authorId, publishedAt, batch);
                 String messageKey = postId + "-" + batchNumber;
                 kafkaPostProducer.sendEventToTopic(event, messageKey, topicName);
-                log.info("Sent FeedPostEvent for postId {} batch {} with subscribers {}", postId, batchNumber, batch);
-                batchNumber.getAndIncrement();
-            });
+                log.info("Sent FeedPostEvent for postId {} batch {} with subscribers {}",
+                        postId, batchNumber, subscribersIds);
+                batchNumber++;
+            }
         }
     }
 
