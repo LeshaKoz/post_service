dfb8630697a012c5e695c4877b28fe37519ce31f
Pavel Chernobay <mr.chernobay1990@gmail.com>
2025-04-04 01:26:51 +0300

    подключил AI орфография и написал тесты

--- src/main/java/faang/school/postservice/service/PostService.java
+++ src/main/java/faang/school/postservice/service/PostService.java
@@ -5,7 +5,11 @@ import faang.school.postservice.model.Post;
 import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -14,6 +18,7 @@ public class PostService {
 
     private static final String POST = "Post";
 
+    private final GrammarBotService grammarBotService;
     private final PostRepository postRepository;
 
     public Post getPost(Long postId) {
@@ -22,5 +27,20 @@ public class PostService {
 
         log.info("Get post with id {}", postId);
         return post;
+    }
+
+    @Retryable(value = Exception.class, maxAttempts = 3, backoff = @Backoff(delay = 2000, multiplier = 2))
+    public void correctUnpublishedPosts() {
+        List<Post> posts = postRepository.findReadyToPublish();
+
+        for (Post post : posts) {
+            try {
+                String correctedText = grammarBotService.checkGrammar(post.getContent());
+                post.setContent(correctedText);
+                postRepository.save(post);
+            } catch (Exception e) {
+                log.error("Error checking post {}: {}", post.getId(), e.getMessage());
+            }
+        }
     }
 }
