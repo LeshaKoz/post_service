60d1bf43aa464e5172e40ed4c5efc14dcaeb35a0
VladimirGontarenko <mister.gontarenko@yandex.ru>
2025-04-05 00:47:42 +0400

    fix-редис

--- src/main/java/faang/school/postservice/service/feed/UserRedisDtoDataService.java
+++ src/main/java/faang/school/postservice/service/feed/UserRedisDtoDataService.java
@@ -3,18 +3,19 @@ package faang.school.postservice.service.feed;
 import faang.school.postservice.client.UserServiceClient;
 import faang.school.postservice.dto.user.UserRedisDto;
 import faang.school.postservice.model.Comment;
+import faang.school.postservice.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserRedisDtoDataService {
-    private final UserServiceClient userServiceClient;
+    private final PostRepository postRepository;
     public UserRedisDto fetchUserInfo(Comment comment) {
 
         long authorId = comment.getAuthorId();
-        String username = userServiceClient.getUser(authorId).username();
-        String email = userServiceClient.getUser(authorId).email();
+        String username = postRepository.getUser(authorId).username();
+        String email = postRepository.getUser(authorId).email();
 
         return UserRedisDto.builder()
                 .id(authorId)
--- src/test/java/faang/school/postservice/service/comment/CommentEventPublisherTest.java
+++ src/test/java/faang/school/postservice/service/comment/CommentEventPublisherTest.java
@@ -1,44 +1,39 @@
 package faang.school.postservice.service.comment;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import faang.school.postservice.dto.event.CommentEventDto;
 import faang.school.postservice.mapper.comment.CommentEventMapper;
 import faang.school.postservice.model.Comment;
+import faang.school.postservice.service.feed.AsyncAddAuthorCommentService;
 import faang.school.postservice.service.kafka.KafkaCommentEventPublisher;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
-public class CommentEventPublisherTest {
+class KafkaCommentEventPublisherTest {
+
     @Mock
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     @Mock
-    private CommentEventMapper commentMapper;
+    private CommentEventMapper commentEventMapper; // Исправлено!
+
+    @Mock
+    private AsyncAddAuthorCommentService asyncAddAuthorCommentService;
 
     @InjectMocks
     private KafkaCommentEventPublisher publisher;
-
-    @Captor
-    private ArgumentCaptor<String> topicCaptor;
-
-    @Captor
-    private ArgumentCaptor<String> keyCaptor;
-
-    @Captor
-    private ArgumentCaptor<Object> valueCaptor;
 
     @Test
     void testPublishCommentEvent() {
@@ -48,19 +43,25 @@ public class CommentEventPublisherTest {
         CommentEventDto dto = new CommentEventDto();
 
         ReflectionTestUtils.setField(publisher, "topic", expectedTopic);
-        when(commentMapper.toDto(comment)).thenReturn(dto);
+
+        when(commentEventMapper.toDto(comment)).thenReturn(dto);
 
         publisher.publishCommentEvent(comment);
 
+        ArgumentCaptor<String> topicCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> keyCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<CommentEventDto> valueCaptor = ArgumentCaptor.forClass(CommentEventDto.class);
+
         verify(kafkaTemplate).send(
-            topicCaptor.capture(),
-            keyCaptor.capture(),
-            valueCaptor.capture()
+                topicCaptor.capture(),
+                keyCaptor.capture(),
+                valueCaptor.capture()
         );
 
         assertEquals(expectedTopic, topicCaptor.getValue());
         assertEquals("123", keyCaptor.getValue());
         assertSame(dto, valueCaptor.getValue());
-        verify(commentMapper, times(1)).toDto(comment);
+        verify(commentEventMapper, times(1)).toDto(comment); // Исправлено!
+        verify(asyncAddAuthorCommentService).addAuthorByCommentToCashAsync(comment);
     }
 }
