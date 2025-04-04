5e9cfff7f8fca8fc74463a5a806d0e15c0c00843
Артемий <xkmitx@gmail.com>
2025-04-04 22:34:52 +0300

    add cron application.yaml, Liquibase add v0005, add column post verified, create RedisConfig, UserModerationService

--- src/main/java/faang/school/postservice/model/Post.java
+++ src/main/java/faang/school/postservice/model/Post.java
@@ -82,4 +82,8 @@ public class Post {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "verified", nullable = false)
+    @Builder.Default
+    private boolean verified = false;
 }
